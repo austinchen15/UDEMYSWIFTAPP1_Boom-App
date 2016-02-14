d4e12042fb08ba61761f98fa281b88e85d7cc00f
austinchen15 <austin581@gmail.com>
2016-02-14 15:42:08 -0600

    added files for boom app

---
+++ Boom App/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Boom App
+//
+//  Created by Austin Chen on 1/22/16.
+//  Copyright © 2016 Austin Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLogo: UIImageView!
+    
+    
+    @IBOutlet weak var coolBg: UIImageView!
+    
+    @IBOutlet weak var uncoolButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBg.hidden = false
+        uncoolButton.hidden = true
+    }
+
+}
+
